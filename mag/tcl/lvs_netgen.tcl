1754fc30b79d6c0cf1c1ed4343ca071b6cea1b00
Matt Venn <matt@mattvenn.net>
2026-06-09 17:26:32 +0200

    try adding a resistor

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -1,6 +1,6 @@
 set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
-readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
+readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.tech/ngspice/sm141064.ngspice $source
 
 # top level GL verilog
 readnet verilog ../src/project.v $source
